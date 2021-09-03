0eee7ce5a61558608c7a7e79be64c11c0f6ca591
kttyk <86214173+kttyk@users.noreply.github.com>
2021-09-03 22:17:59 +0900

    20210903

--- test006.java
+++ test006.java
@@ -1,12 +1,16 @@
-package sample003;
+package sample001;
 
 public class test006 {
 	public static void main(String[] args) {
-		int i = -2;
-		while(i <= 25){
-	    System.out.print(i + ",");
-	    i += 3;
-		}
-     	  
+	String str = "programing";
+	int count =0;
+	 
+	String[] n=str.split("");
+	for(int i=0; i<n.length; i++){
+	  if("a".equals(n[i])){
+	    count++;
+	  }
+	}
+  System.out.println(count);
 	}
 }
--- test007.java
+++ test007.java
@@ -1,14 +1,13 @@
-package sample003;
+package sample001;
 
 public class test007 {
 	public static void main(String[] args) {
-		int i = 1;
-		int count =0;
-		while(i < 32768){
-		  i *= 2;
-		  i++;
-		  count++;
+		int year = 0;
+		double tax = 1.05;
+		for(int i=10000; i<=20000; i++){
+		  i *= tax;
+		  year++;
 		}
-   System.out.println(count);
+    System.out.println("20000円を超えるのは" + year + "年後です。");
 	}
 }
