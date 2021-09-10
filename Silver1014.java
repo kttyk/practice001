deb4e2d7214f2401b660a3f389c71e0f7da802a9
kttyk <86214173+kttyk@users.noreply.github.com>
2021-09-10 23:28:09 +0900

    20210910

---
+++ Silver1014.java
@@ -0,0 +1,9 @@
+package silver;
+
+public class Silver1014 {
+	public static void main(String[] args) {
+		String str = " ";
+		str.trim();
+		System.out.println(str.equals("") + ":" + str.isEmpty());
+	}
+}
---
+++ Silver1017.java
@@ -0,0 +1,10 @@
+package silver;
+
+public class Silver1017 {
+   public static void main(String[] args) {
+	   StringBuilder sb = new StringBuilder();
+	   sb.append("Java");
+	   sb.insert(0,"Hello ");
+	   System.out.println(sb);
+   }
+}
